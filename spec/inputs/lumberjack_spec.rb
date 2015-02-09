0cc3fbeaec23a8933c60ccf4e70ee486992d2081
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-02-09 14:10:06 +0100

    make codec server/connection independant, so there is no mix when multiline is used
    added a custom logstash lumberjack server that takes also cre of the codec.cloning including a first iteration of test
    
    Added test for the usage of a multilne codec throw cloneing the exsiting
    codec when the server receive a run call.
    
    code restyle
    
    fix the codec clone to be done per connection
    
    add @jordansissel patch to handle exceptions in here
    
    Fixes #5
    
    Fixes #1

--- spec/inputs/lumberjack_spec.rb
+++ spec/inputs/lumberjack_spec.rb
@@ -1,1 +1,57 @@
-require "logstash/devutils/rspec/spec_helper"
+# encoding: utf-8
+require "spec_helper"
+require "stud/temporary"
+require 'logstash/inputs/lumberjack'
+require "logstash/codecs/plain"
+require "logstash/codecs/multiline"
+
+describe LogStash::Inputs::Lumberjack do
+
+  let(:ssl_cert) { Stud::Temporary.pathname("ssl_certificate") }
+  let(:ssl_key)  { Stud::Temporary.pathname("ssl_key") }
+
+  let(:queue)  { Queue.new }
+  let(:config)   { { "port" => 0, "ssl_certificate" => ssl_cert, "ssl_key" => ssl_key, "type" => "example" } }
+
+
+  before do
+    system("openssl req -x509  -batch -nodes -newkey rsa:2048 -keyout #{ssl_key} -out #{ssl_cert} -subj /CN=localhost > /dev/null 2>&1")
+  end
+
+  context "#register" do
+
+    it "raise no exception" do
+      plugin = LogStash::Inputs::Lumberjack.new(config)
+      expect { plugin.register}.not_to raise_error
+    end
+  end
+
+  describe "#processing of events" do
+
+    context "#codecs" do
+
+      let(:config) do
+        { "port" => 6969, "ssl_certificate" => ssl_cert, "ssl_key" => ssl_key,
+          "type" => "example", "codec" => codec }
+      end
+
+      let(:codec) { LogStash::Codecs::Multiline.new("pattern" => '\n', "what" => "previous") }
+      subject!(:lumberjack) { LogStash::Inputs::Lumberjack.new(config) }
+      let(:connection) { double("connection") }
+      let(:lines) { {"line" => "one\ntwo\n  two.2\nthree\n"} }
+
+      before(:each) do
+        allow(connection).to receive(:run).and_yield(lines)
+        lumberjack.register
+      end
+
+      it "clone the codec per connection" do
+        expect_any_instance_of(Lumberjack::Server).to receive(:accept).and_return(connection)
+        expect(lumberjack.codec).to receive(:clone).once
+        expect(lumberjack).to receive(:invoke).and_throw(:msg)
+        lumberjack.run(queue)
+      end
+
+    end
+  end
+end
