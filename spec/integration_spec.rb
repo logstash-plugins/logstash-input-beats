59bcdee60e8f4e9a9acf8bf5ca4a775d902c3e1a
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-07-30 15:53:44 -0400

    Cover when the certicate on the client doesnt match the server

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -8,29 +8,15 @@ require "thread"
 require "spec_helper"
 
 describe "A client" do
-  let(:port) { Flores::Random.integer(1024..65335) }
-  let(:csr) { Flores::PKI::CertificateSigningRequest.new }
-  let(:key_bits) { 1024 }
-  let(:key) { OpenSSL::PKey::RSA.generate(key_bits, 65537) }
-  let(:certificate_duration) { Flores::Random.number(1..86400) }
-  let(:certificate_subject) { "CN=server.example.com" }
-  let(:certificate) { csr.create }
+  let(:certificate) { Flores::PKI.generate }
   let(:certificate_file_crt) { "certificate.crt" }
   let(:certificate_file_key) { "certificate.key" }
+  let(:port) { Flores::Random.integer(1024..65335) }
   let(:host) { "127.0.0.1" }
-  let(:queue) { Queue.new }
-  let(:payload) { {"line" => "hello world" } }
 
   before do
-    csr.subject = certificate_subject
-    csr.public_key = key.public_key
-    csr.start_time = Time.now
-    csr.expire_time = csr.start_time + certificate_duration
-    csr.signing_key = key
-    csr.want_signature_ability = true
-
-    expect(File).to receive(:read).twice.with(certificate_file_crt) { certificate.to_s }
-    expect(File).to receive(:read).with(certificate_file_key) { key.to_s }
+    expect(File).to receive(:read).at_least(1).with(certificate_file_crt) { certificate.first.to_s }
+    expect(File).to receive(:read).at_least(1).with(certificate_file_key) { certificate.last.to_s }
     
     server = Lumberjack::Server.new(:port => port,
                                     :address => host,
@@ -38,20 +24,38 @@ describe "A client" do
                                     :ssl_key => certificate_file_key)
 
     Thread.new do
-      server.run { |data| queue.push(data) }
+      server.run { |data| }
     end
   end
 
-  it "should require a certificate" do
-    expect { 
-      client = Lumberjack::Client.new(:port => port, 
-                             :host => host,
-                             :addresses => host,
-                             :ssl_certificate => certificate_file_crt)
+  context "with a valid certificate" do
+    it "successfully connect to the server" do
+      expect { 
+        Lumberjack::Client.new(:port => port, 
+                               :host => host,
+                               :addresses => host,
+                               :ssl_certificate => certificate_file_crt)
 
-      client.write(payload)
-    }.not_to raise_error
+      }.not_to raise_error
+    end
+  end
 
-    expect(queue.pop).to eq(payload)
+  context "with an invalid certificate" do
+    let(:invalid_certificate) { Flores::PKI.generate }
+    let(:invalid_certificate_file) { "invalid.crt" }
+
+    before do
+      expect(File).to receive(:read).with(invalid_certificate_file) { invalid_certificate.first.to_s }
+    end
+
+    it "should refuse to connect" do
+      expect { 
+        Lumberjack::Client.new(:port => port, 
+                               :host => host,
+                               :addresses => host,
+                               :ssl_certificate => invalid_certificate_file)
+
+      }.to raise_error(OpenSSL::SSL::SSLError, /certificate verify failed/)
+    end
   end
 end
