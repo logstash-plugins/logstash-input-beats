5cc0df411b10275a62d798102ce702a2eb90f21a
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-10-28 14:26:32 -0400

    Adding a test for reraise exception

--- spec/lumberjack/beats/connection_spec.rb
+++ spec/lumberjack/beats/connection_spec.rb
@@ -13,11 +13,12 @@ describe "Connnection" do
 
   context "when the server is running" do
     before do
-      expect(socket).to receive(:sysread).at_least(:once).with(Lumberjack::Beats::Connection::READ_SIZE).and_return("")
+      allow(socket).to receive(:sysread).with(Lumberjack::Beats::Connection::READ_SIZE).and_return("")
       allow(socket).to receive(:syswrite).with(anything).and_return(true)
       allow(socket).to receive(:close)
+    end
 
-
+    it "should ack the end of a sequence" do
       expectation = receive(:feed)
         .with("")
         .and_yield(:version, Lumberjack::Beats::Parser::PROTOCOL_VERSION_1)
@@ -26,11 +27,14 @@ describe "Connnection" do
       random_number_of_events.times { |n| expectation.and_yield(:data, start_sequence + n + 1, payload) }
 
       expect_any_instance_of(Lumberjack::Beats::Parser).to expectation
-    end
-
-    it "should ack the end of a sequence" do
       expect(socket).to receive(:syswrite).with(["1A", random_number_of_events + start_sequence].pack("A*N"))
       connection.read_socket
+    end
+
+    it "should not ignore any exception raised by `#sysread`" do
+      expect(server).to receive(:closed?).and_return(false)
+      expect(connection).to receive(:read_socket).and_raise("Something went wrong")
+      expect { |b| connection.run(&b) }.to raise_error
     end
   end
 
