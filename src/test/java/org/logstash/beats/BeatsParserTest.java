9140944813fd59636b74bcf496b2fa98a508f358
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-07-21 10:37:25 -0400

    Better handling of the payload size for v1 and v2 frame
    It will raise an exception if the json payload size or the field count
    is equal or inferior to 0. This commit also add a unit test for when the
    v1 frames use UTF-8 characters for keys or the values to make sure they
    are correctly encoded in the frame.
    
    The assertions on the expected data was reinforced and will now accept
    any name for the keys.
    
    Fixes: #99

--- src/test/java/org/logstash/beats/BeatsParserTest.java
+++ src/test/java/org/logstash/beats/BeatsParserTest.java
@@ -1,5 +1,6 @@
 package org.logstash.beats;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.embedded.EmbeddedChannel;
@@ -8,6 +9,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -60,6 +62,25 @@ public class BeatsParserTest {
     }
 
     @Test
+    public void testEncodingDecodingFieldWithUTFCharacters() {
+        this.batch = new Batch();
+        this.batch.setProtocol(Protocol.VERSION_2);
+
+        // Generate Data with Keys and String with UTF-8
+        for(int i = 0; i < numberOfMessage; i++) {
+            Map map = new HashMap<String, String>();
+            map.put("étoile", "mystère");
+            map.put("from", "ÉeèAççï");
+
+            Message message = new Message(i + 1, map);
+            this.batch.addMessage(message);
+        }
+
+        Batch decodedBatch = decodeBatch();
+        assertMessages(this.batch, decodedBatch);
+    }
+
+    @Test
     public void testCompressedEncodingDecodingFields() {
         this.batch.setProtocol(Protocol.VERSION_1);
         Batch decodedBatch = decodeCompressedBatch();
@@ -68,13 +89,86 @@ public class BeatsParserTest {
 
     @Test
     public void testShouldNotCrashOnGarbageData() {
+        thrown.expectCause(isA(BeatsParser.InvalidFrameProtocolException.class));
+
         byte[] n = new byte[10000];
         new Random().nextBytes(n);
         ByteBuf randomBufferData = Unpooled.wrappedBuffer(n);
 
-        thrown.expectCause(isA(BeatsParser.InvalidFrameProtocol.class));
+        sendPayloadToParser(randomBufferData);
+    }
+
+
+    @Test
+    public void testNegativeJsonPayloadShouldRaiseAnException() throws JsonProcessingException {
+        sendInvalidJSonPayload(-1);
+    }
+
+    @Test
+    public void testZeroSizeJsonPayloadShouldRaiseAnException() throws JsonProcessingException {
+        sendInvalidJSonPayload(0);
+    }
+
+    @Test
+    public void testNegativeFieldsCountShouldRaiseAnException() {
+        sendInvalidV1Payload(-1);
+    }
+
+    @Test
+    public void testZeroFieldsCountShouldRaiseAnException() {
+        sendInvalidV1Payload(0);
+    }
+
+    private void sendInvalidV1Payload(int size) {
+        thrown.expectCause(isA(BeatsParser.InvalidFrameProtocolException.class));
+        thrown.expectMessage("Invalid number of fields, received: " + size);
+
+        ByteBuf payload = Unpooled.buffer();
+
+        payload.writeByte(Protocol.VERSION_1);
+        payload.writeByte('W');
+        payload.writeInt(1);
+        payload.writeByte(Protocol.VERSION_1);
+        payload.writeByte('D');
+        payload.writeInt(1);
+        payload.writeInt(size);
+
+        byte[] key = "message".getBytes();
+        byte[] value = "Hola".getBytes();
+
+        payload.writeInt(key.length);
+        payload.writeBytes(key);
+        payload.writeInt(value.length);
+        payload.writeBytes(value);
+
+        sendPayloadToParser(payload);
+    }
+
+    private void sendInvalidJSonPayload(int size) throws JsonProcessingException {
+        thrown.expectCause(isA(BeatsParser.InvalidFrameProtocolException.class));
+        thrown.expectMessage("Invalid json length, received: " + size);
+
+        Map mapData = Collections.singletonMap("message", "hola");
+
+        ByteBuf payload = Unpooled.buffer();
+
+        payload.writeByte(Protocol.VERSION_2);
+        payload.writeByte('W');
+        payload.writeInt(1);
+        payload.writeByte(Protocol.VERSION_2);
+        payload.writeByte('J');
+        payload.writeInt(1);
+        payload.writeInt(size);
+
+        byte[] json = BeatsParser.MAPPER.writeValueAsBytes(mapData);
+        payload.writeBytes(json);
+
+        sendPayloadToParser(payload);
+    }
+
+    private void sendPayloadToParser(ByteBuf payload) {
         EmbeddedChannel channel = new EmbeddedChannel(new BeatsParser());
-        channel.writeOutbound(randomBufferData);
+        channel.writeOutbound(payload);
         Object o = channel.readOutbound();
         channel.writeInbound(o);
     }
@@ -85,8 +179,19 @@ public class BeatsParserTest {
 
         for(int i=0; i < expected.size(); i++) {
             assertEquals(expected.getMessages().get(i).getSequence(), actual.getMessages().get(i).getSequence());
-            assertEquals(expected.getMessages().get(i).getData().get("line"), actual.getMessages().get(i).getData().get("line"));
-            assertEquals(expected.getMessages().get(i).getData().get("from"), actual.getMessages().get(i).getData().get("from"));
+
+            Map expectedData = expected.getMessages().get(i).getData();
+            Map actualData = actual.getMessages().get(i).getData();
+
+            assertEquals(expectedData.size(), actualData.size());
+
+            for(Object entry : expectedData.entrySet()) {
+                Map.Entry e = (Map.Entry) entry;
+                String key = (String) e.getKey();
+                String value = (String) e.getValue();
+
+                assertEquals(value, actualData.get(key));
+            }
         }
     }
 
