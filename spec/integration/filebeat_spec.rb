55b9f09648167864f1ef3bd77ecfd292309cdd90
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-09-22 13:00:08 -0400

    really test what is goign on travis
    Fixes #142

--- spec/integration/filebeat_spec.rb
+++ spec/integration/filebeat_spec.rb
@@ -50,7 +50,7 @@ describe "Filebeat", :integration => true do
   let_tmp_file(:filebeat_config_path) { YAML.dump(filebeat_config) }
   before :each do
     start_client
-    sleep(10) # give some time to FB to send something
+    sleep(20) # give some time to FB to send something
   end
 
   after :each do
