2a12e6acc712bf9522704876d968a9bd804abd0c
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-09-23 11:23:43 -0400

    Fix an issue when a rejectedError was raise by the executor
    The problem was the order of the shutdown was not correctly defined and
    was async, this PR change the flow to wait for the workgroup and
    bossgroup executor to be closed before terminating the idlestate
    executor.
    
    Also I've changed the parameters of shutdownGracefully to use the
    default method of netty which users saner default values for the quiet
    time and make the code to close more reliably when shutting down
    Logstash.

--- src/main/java/org/logstash/beats/Server.java
+++ src/main/java/org/logstash/beats/Server.java
@@ -27,7 +27,6 @@ import java.util.concurrent.TimeUnit;
 public class Server {
     private final static Logger logger = Logger.getLogger(Server.class);
 
-    static final long SHUTDOWN_TIMEOUT_SECONDS = 10;
     private static final int DEFAULT_CLIENT_TIMEOUT_SECONDS = 15;
 
     private final int port;
@@ -71,10 +70,9 @@ public class Server {
             Channel channel = server.bind(host, port).sync().channel();
             channel.closeFuture().sync();
         } finally {
+            bossGroup.shutdownGracefully().sync();
+            workGroup.shutdownGracefully().sync();
             beatsInitializer.shutdownEventExecutor();
-
-            bossGroup.shutdownGracefully(0, SHUTDOWN_TIMEOUT_SECONDS, TimeUnit.SECONDS);
-            workGroup.shutdownGracefully(0, SHUTDOWN_TIMEOUT_SECONDS, TimeUnit.SECONDS);
         }
 
         return this;
@@ -83,9 +81,9 @@ public class Server {
     public void stop() throws InterruptedException {
         logger.debug("Server shutting down");
 
-        Future<?> bossWait = bossGroup.shutdownGracefully(0, SHUTDOWN_TIMEOUT_SECONDS, TimeUnit.SECONDS);
-        Future<?> workWait = workGroup.shutdownGracefully(0, SHUTDOWN_TIMEOUT_SECONDS, TimeUnit.SECONDS);
-        
+        bossGroup.shutdownGracefully().sync();
+        workGroup.shutdownGracefully().sync();
+
         logger.debug("Server stopped");
     }
 
@@ -148,11 +146,16 @@ public class Server {
 
         @Override
         public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            logger.warn("Channel initializer");
             this.message.onChannelInitializeException(ctx, cause);
         }
 
         public void shutdownEventExecutor() {
-            idleExecutorGroup.shutdownGracefully(0, SHUTDOWN_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+            try {
+                idleExecutorGroup.shutdownGracefully().sync();
+            } catch (InterruptedException e) {
+                // we are shutting down we don't care about any errors here.
+            }
         }
     }
 }
