5c8d6c8a195500f3359377b923caef8addbf0fe7
Christoph Wurm <wurm.ch@gmail.com>
2017-07-10 18:26:00 -0700

    Add cipher for receiving data from logstash-output-lumberjack.
    Fixes #242
    
    Fixes #246

--- src/main/java/org/logstash/netty/SslSimpleBuilder.java
+++ src/main/java/org/logstash/netty/SslSimpleBuilder.java
@@ -50,6 +50,7 @@ public class SslSimpleBuilder {
             "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
             "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384",
             "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
+            "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256",
             "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256"
     };
 
