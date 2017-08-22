89030c6aeb5c124484a73eaf782dd05a815f5497
Guillaume Lefranc <guillaume@signal18.io>
2017-08-22 07:46:06 +0200

    Fix exception not printed in debug log
    Fixes #256

--- lib/logstash/inputs/beats/message_listener.rb
+++ lib/logstash/inputs/beats/message_listener.rb
@@ -64,9 +64,9 @@ module LogStash module Inputs class Beats
       # This is mostly due to a bad certificate or keys, running Logstash in debug mode will show more information
       if cause.is_a?(Java::JavaLang::IllegalArgumentException)
         if input.logger.debug?
-          input.logger.error("Looks like you either have an invalid key or your private key was not in PKCS8 format.")
+          input.logger.error("Looks like you either have a bad certificate, an invalid key or your private key was not in PKCS8 format.", :exception => cause)
         else
-          input.logger.error("Looks like you either have an invalid key or your private key was not in PKCS8 format.", :exception => cause)
+          input.logger.error("Looks like you either have a bad certificate, an invalid key or your private key was not in PKCS8 format.")
         end
       else
         input.logger.warn("Error when creating a connection", :exception => cause.to_s)
