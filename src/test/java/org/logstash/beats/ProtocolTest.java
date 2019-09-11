777acb6c1944ec43faebb26152e2b7749e38fd15
Rob Bavey <rob.bavey@elastic.co>
2019-09-11 11:24:24 -0400

    Improve handling of invalid compressed content
    Also adds frame version checks when reading headers.
    
    Fixes #368

--- src/test/java/org/logstash/beats/ProtocolTest.java
+++ src/test/java/org/logstash/beats/ProtocolTest.java
@@ -1,17 +1,28 @@
 package org.logstash.beats;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
+import static org.hamcrest.Matchers.isA;
+import static org.junit.Assert.assertEquals;
 /**
  * Created by ph on 2016-06-01.
  */
 public class ProtocolTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
-    public void isVersion2Test() {
-        assertTrue(Protocol.isVersion2((byte) '2'));
-        assertFalse(Protocol.isVersion2((byte) '1'));
+    public void validVersionTest() throws InvalidFrameProtocolException{
+        assertEquals(1, Protocol.version((byte) '1'));
+        assertEquals(2, Protocol.version((byte) '2'));
+    }
+
+    @Test
+    public void invalidVersionTest() throws InvalidFrameProtocolException{
+        thrown.expectCause(isA(InvalidFrameProtocolException.class));
+        Protocol.version((byte) '3');
     }
 }
