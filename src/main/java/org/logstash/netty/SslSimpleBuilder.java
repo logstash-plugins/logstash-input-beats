07d770c0307390e5fe7ab5a972e76b0746b35161
Karol Bucek <self@kares.org>
2020-01-13 16:35:53 +0100

    Security: update netty deps - avoid CBC ciphers

--- src/main/java/org/logstash/netty/SslSimpleBuilder.java
+++ src/main/java/org/logstash/netty/SslSimpleBuilder.java
@@ -43,14 +43,10 @@ public class SslSimpleBuilder {
     This list require the OpenSSl engine for netty.
     */
     public final static String[] DEFAULT_CIPHERS = new String[] {
-            "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
-            "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
             "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+            "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
             "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
-            "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384",
-            "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
-            "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256",
-            "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256"
+            "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
     };
 
     private String[] ciphers = DEFAULT_CIPHERS;
