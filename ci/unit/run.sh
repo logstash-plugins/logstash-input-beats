a0f718bb926929edc85cf9282d2ab07fc546e4a0
Joao Duarte <jsvduarte@gmail.com>
2020-01-22 14:45:22 +0100

    try to break existing integration tests

--- ci/unit/run.sh
+++ ci/unit/run.sh
@@ -3,6 +3,6 @@
 # This is intended to be run inside the docker container as the command of the docker-compose.
 set -ex
 
-bundle exec rspec spec
+#bundle exec rspec spec
 bundle exec rake test:integration:setup
 bundle exec rspec spec --tag integration
