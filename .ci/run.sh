34e85273335cb252ac4ab19d02a1751d6be65a55
Joao Duarte <jsvduarte@gmail.com>
2020-03-24 17:06:31 +0000

    update to centralized travis configuration
    update gradle to 5.6
    add custom .ci/Dockerfile due to `openssl` binary needs
    add custom .ci/run.sh for integration testing

--- .ci/run.sh
+++ .ci/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
 # This is intended to be run inside the docker container as the command of the docker-compose.
+
+env
+
 set -ex
 
 bundle exec rspec spec
--- ci/unit/docker-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This is intended to be run inside the docker container as the command of the docker-compose.
-set -ex
-docker-compose -f ci/unit/docker-compose.yml up --exit-code-from logstash
