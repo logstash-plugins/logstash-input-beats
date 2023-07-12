ea7f14dcd5f6e772d99781a833d0f5022f2da634
andsel <selva.andre@gmail.com>
2023-07-12 14:52:51 +0200

    Update the flow control handler to avoid new reads if the channel becomes not writable, to excert backpressure to the sender system

--- src/main/java/org/logstash/beats/FlowLimiterHandler.java
+++ src/main/java/org/logstash/beats/FlowLimiterHandler.java
@@ -1,15 +1,23 @@
 package org.logstash.beats;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Configure the channel where it's installed to operate the read in pull mode,
  * disabling the autoread and explicitly invoking the read operation.
+ * The flow control to keep the outgoing buffer under control is done
+ * avoiding to read in new bytes if the outgoing direction became not writable, this
+ * excert back pressure to the TCP layer and ultimately to the upstream system.
  * */
 @ChannelHandler.Sharable
 public final class FlowLimiterHandler extends ChannelInboundHandlerAdapter {
+
+    private final static Logger logger = LogManager.getLogger(FlowLimiterHandler.class);
 
     @Override
     public void channelRegistered(final ChannelHandlerContext ctx) throws Exception {
@@ -20,7 +28,7 @@ public final class FlowLimiterHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelActive(final ChannelHandlerContext ctx) throws Exception {
         super.channelActive(ctx);
-        if (!ctx.channel().config().isAutoRead()) {
+        if (isAutoreadDisabled(ctx.channel()) && ctx.channel().isWritable()) {
             ctx.channel().read();
         }
     }
@@ -28,8 +36,21 @@ public final class FlowLimiterHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelReadComplete(final ChannelHandlerContext ctx) throws Exception {
         super.channelReadComplete(ctx);
-        if (!ctx.channel().config().isAutoRead()) {
+        if (isAutoreadDisabled(ctx.channel()) && ctx.channel().isWritable()) {
             ctx.channel().read();
         }
     }
+
+    private boolean isAutoreadDisabled(Channel channel) {
+        return !channel.config().isAutoRead();
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        ctx.channel().read();
+        super.channelWritabilityChanged(ctx);
+
+        logger.debug("Writability on channel {} changed to {}", ctx.channel(), ctx.channel().isWritable());
+    }
+
 }
