b9a8cd4d0a00795a623810c7e06968e662162d2f
andsel <selva.andre@gmail.com>
2023-07-17 15:38:08 +0200

    Reshaped the asynch code to be more linear

--- src/test/java/org/logstash/beats/FlowLimiterHandlerTest.java
+++ src/test/java/org/logstash/beats/FlowLimiterHandlerTest.java
@@ -4,22 +4,29 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.PooledByteBufAllocator;
-import io.netty.channel.*;
-import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.SocketChannelConfig;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import org.junit.Test;
 
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 import static org.junit.Assert.*;
 
 public class FlowLimiterHandlerTest {
 
-    private ByteBuf prepareSample(int numBytes) {
+    private ReadMessagesCollector readMessagesCollector;
+
+    private static ByteBuf prepareSample(int numBytes) {
         return prepareSample(numBytes, 'A');
     }
 
@@ -31,15 +38,101 @@ public class FlowLimiterHandlerTest {
         return payload;
     }
 
+    private ChannelInboundHandlerAdapter onClientConnected(Consumer<ChannelHandlerContext> action) {
+        return new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                super.channelActive(ctx);
+                action.accept(ctx);
+            }
+        };
+    }
 
-    boolean anyOtherCall = false;
+    private static class ReadMessagesCollector extends SimpleChannelInboundHandler<ByteBuf> {
+        private Channel clientChannel;
+        private final NioEventLoopGroup group;
+        boolean firstChunkRead = false;
+
+        ReadMessagesCollector(NioEventLoopGroup group) {
+            this.group = group;
+        }
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+            if (!firstChunkRead) {
+                assertEquals("Expect to read a first chunk and no others", 32, msg.readableBytes());
+                firstChunkRead = true;
+
+                // client write other data that MUSTN'T be read by the server, because
+                // is rate limited.
+                clientChannel.writeAndFlush(prepareSample(16)).addListener(new ChannelFutureListener() {
+                    @Override
+                    public void operationComplete(ChannelFuture future) throws Exception {
+                        if (future.isSuccess()) {
+                            // on successful flush schedule a shutdown
+                            ctx.channel().eventLoop().schedule(new Runnable() {
+                                @Override
+                                public void run() {
+                                    group.shutdownGracefully();
+                                }
+                            }, 2, TimeUnit.SECONDS);
+                        } else {
+                            ctx.fireExceptionCaught(future.cause());
+                        }
+                    }
+                });
+
+            } else {
+                // the first read happened, no other reads are commanded by the server
+                // should never pass here
+                fail("Shouldn't never be notified other data while in rate limiting");
+            }
+        }
+
+        public void updateClient(Channel clientChannel) {
+            assertNotNull(clientChannel);
+            this.clientChannel = clientChannel;
+        }
+    }
+
+
+    private static class AssertionsHandler extends ChannelInboundHandlerAdapter {
+
+        private final NioEventLoopGroup group;
+
+        private Throwable lastError;
+
+        public AssertionsHandler(NioEventLoopGroup group) {
+            this.group = group;
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            lastError = cause;
+            group.shutdownGracefully();
+        }
+
+        public void assertNoErrors() {
+            if (lastError != null) {
+                if (lastError instanceof AssertionError) {
+                    throw (AssertionError) lastError;
+                } else {
+                    fail("Failed with error" + lastError);
+                }
+            }
+        }
+    }
+
     @Test
-    public void testIntegration() throws Exception {
+    public void givenAChannelInNotWriteableStateWhenNewBuffersAreSentByClientThenNoDecodeTakePartOnServerSide() throws Exception {
         final int highWaterMark = 32 * 1024;
         FlowLimiterHandler sut = new FlowLimiterHandler();
 
         NioEventLoopGroup group = new NioEventLoopGroup();
         ServerBootstrap b = new ServerBootstrap();
+
+        readMessagesCollector = new ReadMessagesCollector(group);
+        AssertionsHandler assertionsHandler = new AssertionsHandler(group);
         try {
             b.group(group)
                     .channel(NioServerSocketChannel.class)
@@ -48,71 +141,15 @@ public class FlowLimiterHandlerTest {
                         protected void initChannel(SocketChannel ch) throws Exception {
                             ch.config().setWriteBufferHighWaterMark(highWaterMark);
                             ch.pipeline()
-                                    .addLast(new ChannelInboundHandlerAdapter() {
-                                        @Override
-                                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
-                                            super.channelActive(ctx);
-                                            System.out.println("Client connected");
-                                            // write as much to move the channel in not writable state
-                                            final ByteBuf payload = prepareSample(highWaterMark, 'C');
-                                            ByteBuf copy = payload.copy();
-                                            int readableBytes = copy.readableBytes();
-                                            ctx.pipeline().writeAndFlush(copy).addListener(new ChannelFutureListener() {
-                                                @Override
-                                                public void operationComplete(ChannelFuture future) throws Exception {
-                                                    System.out.println("Bytes to be read: " + readableBytes + " now availables:" + copy.readableBytes());
-                                                }
-                                            });
-//                                            System.out.println("Bytes to be read: " + readableBytes + " now:" + copy.readableBytes());
-//                                            payload.release();
-
-                                            System.out.println("Channel isWritable?" + ctx.channel().isWritable());
-                                            int numBuffers = 0;
-                                            while (ctx.channel().isWritable()) {
-                                                ctx.pipeline().writeAndFlush(payload.copy());
-                                                numBuffers ++;
-                                            }
-                                            System.out.println("Num buffers wrote to get the writable false: " + numBuffers);
-                                            System.out.println("Channel isWritable? " + ctx.channel().isWritable() + " high water mark: " + ctx.channel().config().getWriteBufferHighWaterMark());
-                                            // ask the client to send some data present on the channel
-                                            int receiveBufferSize = ((SocketChannelConfig) ctx.channel().config()).getReceiveBufferSize();
-                                            System.out.println("Server's receive buffer size: " + receiveBufferSize);
-                                            clientChannel.writeAndFlush(prepareSample(32));
-                                        }
-                                    })
+                                    .addLast(onClientConnected(ctx -> {
+                                        // write as much to move the channel in not writable state
+                                        fillOutboundWatermark(ctx, highWaterMark);
+                                        // ask the client to send some data present on the channel
+                                        clientChannel.writeAndFlush(prepareSample(32));
+                                    }))
                                     .addLast(sut)
-                                    .addLast(new SimpleChannelInboundHandler<ByteBuf>() {
-                                        boolean firstChunkRead = false;
-
-                                        @Override
-                                        protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
-                                            System.out.println("Last handler Buffer read size: " + msg.readableBytes());
-                                            if (!firstChunkRead) {
-                                                assertEquals("Expect to read a first chunk and no others", 32, msg.readableBytes());
-                                                firstChunkRead = true;
-
-                                                // client write other data that MUSTN'T be read by the server, because
-                                                // is in rate limiting.
-                                                clientChannel.writeAndFlush(prepareSample(16)).addListener(new ChannelFutureListener() {
-                                                    @Override
-                                                    public void operationComplete(ChannelFuture future) throws Exception {
-                                                        // on successful flush schedule a shutdown
-                                                        ctx.channel().eventLoop().schedule(new Runnable() {
-                                                            @Override
-                                                            public void run() {
-                                                             group.shutdownGracefully();
-                                                            }
-                                                        }, 2, TimeUnit.SECONDS);
-                                                    }
-                                                });
-
-                                            } else {
-                                                // the first read happened, no other reads are commanded by the server
-                                                // should never pass here
-                                                anyOtherCall = true;
-                                            }
-                                        }
-                                    });
+                                    .addLast(readMessagesCollector)
+                                    .addLast(assertionsHandler);
                         }
                     });
             ChannelFuture future = b.bind("0.0.0.0", 1234).addListener(new ChannelFutureListener() {
@@ -128,7 +165,14 @@ public class FlowLimiterHandlerTest {
             group.shutdownGracefully().sync();
         }
 
-        assertFalse("Shouldn't never be notified other data while in rate limiting", anyOtherCall);
+        assertionsHandler.assertNoErrors();
+    }
+
+    private static void fillOutboundWatermark(ChannelHandlerContext ctx, int highWaterMark) {
+        final ByteBuf payload = prepareSample(highWaterMark, 'C');
+        while (ctx.channel().isWritable()) {
+            ctx.pipeline().writeAndFlush(payload.copy());
+        }
     }
 
     Channel clientChannel;
@@ -142,6 +186,7 @@ public class FlowLimiterHandlerTest {
                     protected void initChannel(SocketChannel ch) throws Exception {
                         ch.config().setAutoRead(false);
                         clientChannel = ch;
+                        readMessagesCollector.updateClient(clientChannel);
                     }
                 });
         b.connect("localhost", 1234);
